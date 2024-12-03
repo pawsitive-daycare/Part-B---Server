261156febb1808b4e08d34ea6a91143ffb6c92da
John Niduelan <jeniduelan@gmail.com>
2024-12-03 20:51:12 +1100

    feat: pet profile model done

--- src/models/booking.js
+++ src/models/booking.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.ObjectId,
+        ref: 'User'
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    
+})
--- src/models/petProfile.js
+++ src/models/petProfile.js
@@ -1,1 +1,28 @@
 const mongoose = require("mongoose");
+
+const petProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.ObjectId,
+        ref: 'User'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    animal: {
+        type: String,
+        required: true
+    },
+    breed: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+});
+const PetProfile = mongoose.model('PetProfile', petProfileSchema);
+module.exports = {
+    PetProfile
+}
